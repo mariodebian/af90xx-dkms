3ef913468be0840a0b4a820c0b16872cc2a9adec
Mario Izquierdo (mariodebian) <mariodebian@gmail.com>
2011-11-17 02:04:49 +0100

    Add script to compare files with kernel source

---
+++ compare.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+DIR=/usr/src/linux-source-3.1/drivers/media/
+
+cd dvb
+
+for f in *; do
+
+  NEW=$(find ${DIR} -name "$f")
+  if [ -f "${NEW}" ]; then
+
+    if ! diff "$NEW" "$f" >/dev/null 2>&1 ; then
+      #diff -ur "$NEW" "$f"
+      echo meld "$NEW" "dvb/$f"
+    else
+      echo "SAME FILE $NEW dvb/$f"
+    fi
+
+  fi
+
+done
